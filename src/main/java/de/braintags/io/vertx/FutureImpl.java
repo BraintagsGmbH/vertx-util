8c2deaf3c61f5cfe1033cb915e5e73d36f32f732
remi128 <mremme@braintags.de>
2016-01-22 12:46:55 +0100

    init

---
+++ src/main/java/de/braintags/io/vertx/FutureImpl.java
@@ -0,0 +1,20 @@
+package de.braintags.io.vertx;
+
+import io.vertx.core.Future;
+
+/**
+ * 
+ * 
+ * 
+ * @author mremme
+ *
+ */
+public class FutureImpl implements Future<T> {
+
+  /**
+   * 
+   */
+  public FutureImpl() {
+  }
+
+}
