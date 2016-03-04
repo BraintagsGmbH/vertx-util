0375941f5e5b99822853b2d85f5ec426f71fff39
remi128 <mremme@braintags.de>
2016-03-04 11:52:22 +0100

    added doc

---
+++ src/main/java/de/braintags/io/vertx/util/package-info.java
@@ -0,0 +1,40 @@
+/*
+ * #%L vertx-pojo-mapper-common %% Copyright (C) 2015 Braintags GmbH %% All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html #L%
+ */
+/**
+ * :numbered:
+ * :toc: left
+ * :toclevels: 3
+ * 
+ * = vertx-util
+ * utilities and helper for vertx-based programming
+ * 
+ * === Using vertx-util
+ * To use this project, add the following dependency to the _dependencies_ section of your build descriptor:
+ * 
+ * * Maven (in your `pom.xml`):
+ *
+ * [source,xml,subs="+attributes"]
+ * ----
+ * <dependency>
+ * <groupId>de.braintags</groupId>
+ * <artifactId>{maven.artifactId}</artifactId>
+ * <version>{maven.version}</version>
+ * </dependency>
+ * ----
+ *
+ * * Gradle (in your `build.gradle` file):
+ *
+ * [source,groovy,subs="+attributes"]
+ * ----
+ * compile "de.braintags:{maven.artifactId}:{maven.version}"
+ * ----
+ *
+ *
+ */
+@Document(fileName = "index.adoc")
+package de.braintags.io.vertx.util;
+
+import io.vertx.docgen.Document;
