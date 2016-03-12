70880d9d70f998a8e3ab0f39974b022d7999c920
remi128 <mremme@braintags.de>
2016-03-12 16:34:27 +0100

    stream objects - not usable yet

---
+++ src/main/java/de/braintags/io/vertx/util/file/BufferInputStream.java
@@ -0,0 +1,57 @@
+/*
+ * #%L
+ * vertx-util
+ * %%
+ * Copyright (C) 2015 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package de.braintags.io.vertx.util.file;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import io.vertx.core.buffer.Buffer;
+
+/**
+ * 
+ * 
+ * @author Michael Remme
+ * 
+ */
+public class BufferInputStream extends InputStream {
+  private Buffer buffer;
+  /**
+   * The index of the next character to read from the input stream buffer.
+   *
+   */
+  protected int pos;
+
+  /**
+   * The number of valid characters in the input stream buffer.
+   *
+   */
+  protected int count;
+
+  /**
+   * 
+   */
+  public BufferInputStream(Buffer buffer) {
+    this.buffer = buffer;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.io.InputStream#read()
+   */
+  @Override
+  public int read() throws IOException {
+    return (pos < count) ? (buffer.getByte(pos++) & 0xFF) : -1;
+  }
+
+}
