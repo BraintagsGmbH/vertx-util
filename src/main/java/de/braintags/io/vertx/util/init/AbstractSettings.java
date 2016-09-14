dcc59323fd8d4e0e9816769c039edc9289c19378
Michael Remme <mremme@braintags.de>
2016-09-14 18:11:50 +0200

    use composite future

---
+++ src/main/java/de/braintags/io/vertx/util/init/AbstractSettings.java
@@ -0,0 +1,35 @@
+/*
+ * #%L
+ * vertx-util
+ * %%
+ * Copyright (C) 2016 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+package de.braintags.io.vertx.util.init;
+
+/**
+ * An abstract class dealing with settings
+ * 
+ * @author Michael Remme
+ * 
+ */
+public class AbstractSettings {
+  private static final io.vertx.core.logging.Logger LOGGER = io.vertx.core.logging.LoggerFactory
+      .getLogger(AbstractSettings.class);
+
+  private String settingsName;
+
+  /**
+   * 
+   * @param settingsName
+   */
+  public AbstractSettings(String settingsName) {
+    this.settingsName = settingsName;
+  }
+
+}
