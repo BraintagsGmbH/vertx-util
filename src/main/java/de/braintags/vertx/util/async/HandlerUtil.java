45b8d6fe2cd70b08ca247d7f2b3c0ecd516b12b1
Stefan Schmitt <stefan.schmitt@braintags.de>
2017-03-08 18:09:03 +0100

    Added utility class for vertx handlers

---
+++ src/main/java/de/braintags/vertx/util/async/HandlerUtil.java
@@ -0,0 +1,47 @@
+package de.braintags.vertx.util.async;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+
+/**
+ * Utility class for vertx {@link Handler Handlers}
+ * 
+ * @author sschmitt
+ * 
+ */
+public class HandlerUtil {
+
+  private HandlerUtil() {
+  }
+
+  /**
+   * Forwards the result of a handler with an object return type to a void handler, omitting the result and simply
+   * returning the success or failure.
+   * Useful if you have a handler:
+   * <p>
+   * <code>Handler&lt;Asyncresult&lt;Void&gt;&gt; voidHandler</code>
+   * </p>
+   * and call a method:
+   * <p>
+   * <code>doSomething(Handler&lt;Asyncresult&lt;Object&gt;&gt; objectHandler)</code>
+   * </p>
+   * and simply want to forward the result of the method to your handler, you can use:
+   * <p>
+   * <code>doSomething(HandlerUtil.voidCompleter(voidHandler))</code>
+   * </p>
+   * 
+   * @param handler
+   *          your void handler
+   * @return a handler for any object that forwards the success or failure to your void handler
+   */
+  public static <T> Handler<AsyncResult<T>> voidCompleter(Handler<AsyncResult<Void>> handler) {
+    return result -> {
+      if (result.failed())
+        handler.handle(Future.failedFuture(result.cause()));
+      else
+        handler.handle(Future.succeededFuture());
+    };
+  }
+
+}
