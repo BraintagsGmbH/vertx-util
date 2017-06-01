b8ec3b1dbb4871f5317877d54df4ea448edd24f9
Martin Pluecker <martin.pluecker@braintags.de>
2017-06-01 13:23:42 +0200

    added license headers

--- src/main/java/de/braintags/vertx/util/json/DefaultingTypeResolver.java
+++ src/main/java/de/braintags/vertx/util/json/DefaultingTypeResolver.java
@@ -1,3 +1,15 @@
+/*
+ * #%L
+ * Vert.x utilities from Braintags
+ * %%
+ * Copyright (C) 2017 Braintags GmbH
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
 package de.braintags.vertx.util.json;
 
 import java.util.Collection;
