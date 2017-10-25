21bd1aea4605d91c5dd4d03bd69c0b6118615278
Martin Pluecker <martin.pluecker@braintags.de>
2017-10-25 17:57:22 +0200

    work on authentication

--- src/main/java/de/braintags/vertx/util/async/CacheableFuture.java
+++ src/main/java/de/braintags/vertx/util/async/CacheableFuture.java
@@ -13,7 +13,7 @@ import io.vertx.core.Handler;
  *
  * @param <T>
  */
-public interface CacheableFuture<T> extends Future<T>, CacheableResult<T> {
+public interface CacheableFuture<T> extends SharedFuture<T>, CacheableResult<T> {
 
   public static <T> CacheableFuture<T> toCacheable(final Future<T> future) {
     if (future instanceof CacheableFuture) {
@@ -44,94 +44,18 @@ public interface CacheableFuture<T> extends Future<T>, CacheableResult<T> {
   Handler<CacheableResult<T>> cacheHandler();
 
   @Override
-  default <U> CacheableFuture<U> compose(final Function<T, Future<U>> mapper) {
-    if (mapper == null) {
-      throw new NullPointerException();
-    }
-    CacheableFuture<U> ret = CacheableFuture.future();
-    setHandler(ar -> {
-      ret.reduceExpire(this.expires());
-      if (ar.succeeded()) {
-        Future<U> apply;
-        try {
-          apply = mapper.apply(ar.result());
-        } catch (Throwable e) {
-          ret.fail(e);
-          return;
-        }
-        apply.setHandler(ret);
-      } else {
-        ret.fail(ar.cause());
-      }
-    });
-    return ret;
-  }
+  <U> CacheableFuture<U> compose(final Function<T, Future<U>> mapper);
 
   @Override
-  default <U> CacheableFuture<U> map(final Function<T, U> mapper) {
-    if (mapper == null) {
-      throw new NullPointerException();
-    }
-    CacheableFuture<U> ret = CacheableFuture.future();
-    setHandler(ar -> {
-      ret.reduceExpire(this.expires());
-      if (ar.succeeded()) {
-        U mapped;
-        try {
-          mapped = mapper.apply(ar.result());
-        } catch (Throwable e) {
-          ret.fail(e);
-          return;
-        }
-        ret.complete(mapped);
-      } else {
-        ret.fail(ar.cause());
-      }
-    });
-    return ret;
-  }
+  <U> CacheableFuture<U> map(final Function<T, U> mapper);
 
   @Override
-  default <V> CacheableFuture<V> map(final V value) {
-    CacheableFuture<V> ret = CacheableFuture.future();
-    setHandler(ar -> {
-      ret.reduceExpire(this.expires());
-      if (ar.succeeded()) {
-        ret.complete(value);
-      } else {
-        ret.fail(ar.cause());
-      }
-    });
-    return ret;
-  }
+  <V> CacheableFuture<V> map(final V value);
 
   @Override
-  default CacheableFuture<T> recover(final Function<Throwable, Future<T>> mapper) {
-    if (mapper == null) {
-      throw new NullPointerException();
-    }
-    CacheableFuture<T> ret = CacheableFuture.future();
-    setHandler(ar -> {
-      ret.reduceExpire(this.expires());
-      if (ar.succeeded()) {
-        ret.complete(result());
-      } else {
-        Future<T> mapped;
-        try {
-          mapped = mapper.apply(ar.cause());
-        } catch (Throwable e) {
-          ret.fail(e);
-          return;
-        }
-        mapped.setHandler(ret);
-      }
-    });
-    return ret;
-  }
+  CacheableFuture<T> recover(final Function<Throwable, Future<T>> mapper);
 
   @Override
-  default <V> CacheableFuture<V> mapEmpty() {
-    return map((V) null);
-  }
+  <V> CacheableFuture<V> mapEmpty();
 
 }
