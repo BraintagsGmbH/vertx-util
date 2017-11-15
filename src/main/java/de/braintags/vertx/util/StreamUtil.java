b63a5d11098fac7e8916811ca2de1b7d8998b825
Stefan Schmitt <stefan.schmitt@braintags.de>
2017-11-15 10:04:39 +0100

    Added util class for java streams

---
+++ src/main/java/de/braintags/vertx/util/StreamUtil.java
@@ -0,0 +1,42 @@
+package de.braintags.vertx.util;
+
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class StreamUtil {
+
+  private StreamUtil() {
+
+  }
+
+  /**
+   * A collector that only returns a single element. Throws an {@link IllegalStateException} if the stream has 0 or more
+   * than 1 element.
+   * 
+   * @return the single element of the stream
+   */
+  public static <T> Collector<T, ?, T> singletonCollector() {
+    return Collectors.collectingAndThen(Collectors.toList(), list -> {
+      if (list.size() != 1) {
+        throw new IllegalStateException("Expected exactly one element, but got " + list.size());
+      }
+      return list.get(0);
+    });
+  }
+
+  /**
+   * A collector that returns an optional element, but fails with an {@link IllegalStateException} if the stream
+   * contains more than one element.
+   * 
+   * @return an optional element of the stream
+   */
+  public static <T> Collector<T, ?, Optional<T>> optionalCollector() {
+    return Collectors.collectingAndThen(Collectors.toList(), list -> {
+      if (list.size() > 1) {
+        throw new IllegalStateException("Expected at most one element, but got " + list.size());
+      }
+      return list.isEmpty() ? Optional.empty() : Optional.of(list.get(0));
+    });
+  }
+}
