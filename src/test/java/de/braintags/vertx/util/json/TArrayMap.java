6109b18eaef657ac43f1c443ba2674f289af1a6e
Martin Pluecker <martin.pluecker@braintags.de>
2017-11-22 15:29:22 +0100

    removed debug output

--- src/test/java/de/braintags/vertx/util/json/TArrayMap.java
+++ src/test/java/de/braintags/vertx/util/json/TArrayMap.java
@@ -78,7 +78,6 @@ public class TArrayMap {
     JsonNode valueTreeData = Json.mapper.valueToTree(testData);
 
     JsonNode diff = JsonDiff.getDiff(valueTreeBase, valueTreeData);
-    System.out.println(diff);
 
     JsonNode diffApplied = JsonDiff.applyDiff(valueTreeBase, diff);
     Assert.assertEquals(valueTreeData, diffApplied);
