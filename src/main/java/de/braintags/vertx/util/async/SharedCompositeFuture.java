576537046cb03f8ac7e03bb2572ef3e6cba2f5b1
Martin Pluecker <martin.pluecker@braintags.de>
2018-03-12 14:04:56 +0100

    initializing roles

---
+++ src/main/java/de/braintags/vertx/util/async/SharedCompositeFuture.java
@@ -0,0 +1,23 @@
+package de.braintags.vertx.util.async;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+
+public class SharedCompositeFuture {
+
+  public static SharedFuture<Void> allVoid(final Collection<? extends Future<?>> futures) {
+    return SharedFuture.wrap(CompositeFuture.all(new ArrayList(futures))).mapEmpty();
+  }
+
+  public static <T> SharedFuture<List<T>> all(final List<? extends Future<T>> futures) {
+    return SharedFuture
+        .wrap(CompositeFuture.all((List) futures).map(v -> futures.stream().map(Future::result).collect(toList())));
+  }
+
+}
