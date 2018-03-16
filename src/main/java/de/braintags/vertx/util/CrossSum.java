79d259c51ce50a1df859c84f94a5a5b8cfc1884b
Michael Remme <mremme@braintags.de>
2018-03-16 14:27:26 +0100

    added helper to calc cross sum

---
+++ src/main/java/de/braintags/vertx/util/CrossSum.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package de.braintags.vertx.util;
+
+/**
+ * Helper class to calculate cross sum
+ * 
+ * @author mremme
+ *
+ */
+public class CrossSum {
+  private CrossSum() {
+  }
+
+  /**
+   * Creates the cross sum of the given long
+   * 
+   * @param source
+   * @return
+   */
+  public static long getCrossSum(Long source) {
+    long summe = 0;
+    while (0 != source) {
+      summe = summe + (source % 10);
+      source = source / 10;
+    }
+    return summe;
+  }
+
+
+}
