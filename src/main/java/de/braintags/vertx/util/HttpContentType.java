d21da69c3d884f6fab45a929ba99231dcbfba534
Jan Kerkenhoff <jan.kerkenhoff@braintags.de>
2018-06-06 10:13:41 +0200

    correct contenttypes

--- src/main/java/de/braintags/vertx/util/HttpContentType.java
+++ src/main/java/de/braintags/vertx/util/HttpContentType.java
@@ -12,8 +12,11 @@
  */
 package de.braintags.vertx.util;
 
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.stream.Collectors.toList;
 
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.LinkedHashMap;
@@ -30,39 +33,70 @@ public class HttpContentType {
   public static final String MAIN_TYPE_APPLICATION = "application";
   public static final String MAIN_TYPE_TEXT = "text";
   public static final String MAIN_TYPE_IMAGE = "image";
+  public static final String MAIN_TYPE_MULTIPART = "multipart";
 
   public static final String SUB_TYPE_JSON = "json";
   public static final String SUB_TYPE_JAVASCRIPT = "javascript";
   public static final String SUB_TYPE_HTML = "html";
+  public static final String SUB_TYPE_PLAIN = "plain";
   public static final String SUB_TYPE_CSS = "css";
+  public static final String SUB_TYPE_FORM_URL_ENCODED = "x-www-form-urlencoded";
+  public static final String SUB_TYPE_OCTET_STREAM = "octet-stream";
+  public static final String SUB_TYPE_XHTML = "xhtml+xml";
+  public static final String SUB_TYPE_XML = "xml";
 
   public static final String SUB_TYPE_PNG = "png";
   public static final String SUB_TYPE_SVG = "svg+xml";
   public static final String SUB_TYPE_JPEG = "jpeg";
+  public static final String SUB_TYPE_BMP = "bmp";
+  public static final String SUB_TYPE_GIF = "gif";
+  public static final String SUB_TYPE_TIFF = "tiff";
+  public static final String SUB_TYPE_WEBP = "webp";
+
+  public static final String SUB_TYPE_FORM_DATA = "form-data";
+  public static final String SUB_TYPE_ALTERNATIE = "alternative";
 
   private static final String PARAM_CHARSET = "charset";
 
-  private static final String CHARSET_UTF8 = "utf-8";
-
-  public static final HttpContentType TEXT_HTML = new HttpContentType(MAIN_TYPE_TEXT, SUB_TYPE_HTML);
+  public static final HttpContentType TEXT_HTML = new HttpContentType(MAIN_TYPE_TEXT, SUB_TYPE_PLAIN, ISO_8859_1);
+  public static final HttpContentType TEXT_HTML_UTF8 = new HttpContentType(MAIN_TYPE_TEXT, SUB_TYPE_PLAIN, UTF_8);
+  public static final HttpContentType TEXT_PLAIN = new HttpContentType(MAIN_TYPE_TEXT, SUB_TYPE_HTML, ISO_8859_1);
+  public static final HttpContentType TEXT_XML = new HttpContentType(MAIN_TYPE_TEXT, SUB_TYPE_XML, ISO_8859_1);
   public static final HttpContentType TEXT_CSS = new HttpContentType(MAIN_TYPE_TEXT, SUB_TYPE_CSS);
 
   public static final HttpContentType APPLICATION_JAVASCRIPT = new HttpContentType(MAIN_TYPE_APPLICATION,
       SUB_TYPE_JAVASCRIPT);
-
-  public static final HttpContentType JSON = new HttpContentType(MAIN_TYPE_APPLICATION, SUB_TYPE_JSON);
-  public static final HttpContentType JSON_UTF8 = new HttpContentType(MAIN_TYPE_APPLICATION, SUB_TYPE_JSON,
-      Collections.singletonMap(PARAM_CHARSET, CHARSET_UTF8));
+  public static final HttpContentType APPLICATION_FORM_URLENCODED = new HttpContentType(MAIN_TYPE_APPLICATION,
+      SUB_TYPE_FORM_URL_ENCODED, ISO_8859_1);
+  public static final HttpContentType APPLICATION_OCTET_STREAM = new HttpContentType(MAIN_TYPE_APPLICATION,
+      SUB_TYPE_OCTET_STREAM, ISO_8859_1);
+  public static final HttpContentType APPLICATION_SVG_XML = new HttpContentType(MAIN_TYPE_APPLICATION, SUB_TYPE_SVG,
+      ISO_8859_1);
+  public static final HttpContentType APPLICATION_XHTML_XML = new HttpContentType(MAIN_TYPE_APPLICATION, SUB_TYPE_XHTML,
+      ISO_8859_1);
+  public static final HttpContentType APPLICATION_XML = new HttpContentType(MAIN_TYPE_APPLICATION, SUB_TYPE_XML,
+      ISO_8859_1);
+  public static final HttpContentType APPLICATION_JSON = new HttpContentType(MAIN_TYPE_APPLICATION, SUB_TYPE_JSON,
+      UTF_8);
 
   public static final HttpContentType IMAGE_PNG = new HttpContentType(MAIN_TYPE_IMAGE, SUB_TYPE_PNG);
   public static final HttpContentType IMAGE_SVG = new HttpContentType(MAIN_TYPE_IMAGE, SUB_TYPE_SVG);
   public static final HttpContentType IMAGE_JPEG = new HttpContentType(MAIN_TYPE_IMAGE, SUB_TYPE_JPEG);
+  public static final HttpContentType IMAGE_BMP = new HttpContentType(MAIN_TYPE_IMAGE, SUB_TYPE_BMP);
+  public static final HttpContentType IMAGE_GIF = new HttpContentType(MAIN_TYPE_IMAGE, SUB_TYPE_GIF);
+  public static final HttpContentType IMAGE_WEBP = new HttpContentType(MAIN_TYPE_IMAGE, SUB_TYPE_WEBP);
+  public static final HttpContentType IMAGE_TIFF = new HttpContentType(MAIN_TYPE_IMAGE, SUB_TYPE_TIFF);
+
+  public static final HttpContentType MULTIPART_ALTERNATIVE = new HttpContentType(MAIN_TYPE_MULTIPART, SUB_TYPE_JPEG,
+      ISO_8859_1);
+  public static final HttpContentType MULTIPART_FORM_DATA = new HttpContentType(MAIN_TYPE_MULTIPART, SUB_TYPE_JPEG,
+      ISO_8859_1);
 
   private final String value;
   private final String mainType;
   private final String subType;
   private final Map<String, String> parameters;
-  private final String parameterlessValue;
+  private final String mimeType;
 
   public static HttpContentType parse(final String string) {
     try {
@@ -75,12 +109,17 @@ public class HttpContentType {
 
   private static Map<String, String> toMap(final MimeTypeParameterList parameters) {
     LinkedHashMap<String, String> result = new LinkedHashMap<>();
+    @SuppressWarnings("rawtypes")
     Enumeration names = parameters.getNames();
     while (names.hasMoreElements()) {
       String name = names.nextElement().toString();
       result.put(name, parameters.get(name));
     }
     return result;
+  }
+
+  private HttpContentType(final String mainType, final String subType, final Charset charset) {
+    this(mainType, subType, Collections.singletonMap(PARAM_CHARSET, charset.name().toLowerCase()));
   }
 
   public HttpContentType(final String mainType, final String subType) {
@@ -91,8 +130,8 @@ public class HttpContentType {
     this.mainType = mainType;
     this.subType = subType;
     this.parameters = parameters;
-    parameterlessValue = mainType + "/" + subType;
-    this.value = parameterlessValue + (parameters.isEmpty() ? ""
+    mimeType = mainType + "/" + subType;
+    this.value = mimeType + (parameters.isEmpty() ? ""
         : ";" + Joiner.on(';').join(
             parameters.entrySet().stream().map(param -> param.getKey() + "=" + param.getValue()).collect(toList())));
   }
@@ -102,8 +141,8 @@ public class HttpContentType {
     return value;
   }
 
-  public String getParameterlessValue() {
-    return parameterlessValue;
+  public String getMimeType() {
+    return mimeType;
   }
 
   public String getValue() {
