df129c85c404bef1dfec57dfdd09f7f8043b0d65
Martin Pluecker <martin.pluecker@braintags.de>
2018-07-10 10:20:39 +0200

    improve performance

--- src/main/java/de/braintags/vertx/util/DebugDetection.java
+++ src/main/java/de/braintags/vertx/util/DebugDetection.java
@@ -9,17 +9,23 @@ package de.braintags.vertx.util;
  */
 public class DebugDetection {
 
-  // TODO: only temporary, replace this by some environment detection
+  private static final boolean develop = detectDevelopMode();
+  private static final boolean test = detectTestMode();
+  private static final boolean fileCachingDisabled = detectFileCahcingDisabled();
+
   public static boolean isLaunchedByEclipse() {
-    String develop = System.getProperty("develop");
-    if (develop != null)
-      return "true".equals(develop);
-    else
-      return java.lang.management.ManagementFactory.getRuntimeMXBean().getInputArguments().toString()
-          .indexOf("-agentlib:jdwp") >= 0;
+    return develop;
   }
 
   public static boolean isTest() {
+    return test;
+  }
+
+  public static boolean isFileCachingDisabled() {
+    return fileCachingDisabled;
+  }
+
+  private static boolean detectTestMode() {
     String test = System.getProperty("test");
     if (test != null)
       return "true".equals(test);
@@ -27,8 +33,17 @@ public class DebugDetection {
       return isLaunchedByEclipse();
   }
 
-  public static boolean isFileCachingDisabled() {
+  private static boolean detectFileCahcingDisabled() {
     return "true".equals(System.getProperties().getProperty("vertx.disableFileCaching"));
+  }
+
+  private static boolean detectDevelopMode() {
+    String develop = System.getProperty("develop");
+    if (develop != null)
+      return "true".equals(develop);
+    else
+      return java.lang.management.ManagementFactory.getRuntimeMXBean().getInputArguments().toString()
+          .indexOf("-agentlib:jdwp") >= 0;
   }
 
 }
